c429a01c238bc5c5647b0b35e1953ce342d3793d
isinka <altarika@icloud.com>
2022-10-30 00:01:38 +0300

    add new functions : delete func for phones, change function for phones change file name change logic for strip split

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from add_class import AddressBook, Record
+from address_book_classes import AddressBook, Record
 
 contacts_dict = AddressBook()
 
@@ -65,22 +65,17 @@ def add_func(data):
 
 
 @input_error
-def change_phones_func(data):
+def change_phone_func(data):
     """
     Зміна вже існуючого контактного номера. Взаємодія з класами.
     :param data: Строка з ім'ям та телефоном.
     :return: Відповідь про зміни.
     """
     name, phones = create_data(data)
-    if contacts_dict.has_record(name):
-        record = contacts_dict.get_record(name)
-        record.clear_phones()
+    record = contacts_dict[name]
+    record.change_phones(phones)
 
-        for phone in phones:
-            record.add_phone(phone)
-
-        return f'You changed numbers to {phones} for {name}.'
-    return 'Use add command plz.'
+    return 'Phones were changed.'
 
 
 @input_error
@@ -118,6 +113,14 @@ def del_func(name):
         return "You deleted the contact."
 
     raise ValueError('This contact does not exist.')
+@input_error
+def del_phone_func(data):
+    name, phone = data.strip().split(' ')
+
+    record = contacts_dict[name]
+    if record.delete_phone(phone):
+        return f'Phone {phone} for {name} contact deleted.'
+    return f'{name} contact does not have this number'
 
 
 COMMANDS_DICT = {
@@ -126,9 +129,10 @@ COMMANDS_DICT = {
     'close': exit_func,
     'good bye': exit_func,
     'add': add_func,
-    'change phones': change_phones_func,
+    'change phone': change_phone_func,
     'show all': show_func,
     'phone': search_func,
+    'delete phone': del_phone_func,
     'delete': del_func
 }
 
@@ -158,9 +162,8 @@ def create_data(data):
     :param data: Строка з номером і ім'ям.
     :return: Вже розділені ім'я і номер
     """
-    new_data = data.strip().split(" ")
-    name = new_data[0]
-    phones = new_data[1:]
+    name, *phones = data.strip().split(' ')
+
     if name.isnumeric():
         raise ValueError('Wrong name.')
     for phone in phones:
